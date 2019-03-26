a8607631bcb6c5699062fc5067f2101b225581e5
Steven Chu <work@stevenchustudio.com>
2019-03-26 06:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 
-function theBeatlesPlay(musicians,instruments) { return musicians[0] + "plays the " + instruments[0]
-}
+//function theBeatlesPlay(musicians,instruments) { return musicians[0] + "plays the " + instruments[0]}
 
-
-
-/*
 function theBeatlesPlay(musicians,instruments) {
-  var i = []
+  var output = []
   for(var i = 0;i<musicians.length;i++) {
       return musicians[i] + "plays " + instruments[i]
   }
 }
-*/
+
