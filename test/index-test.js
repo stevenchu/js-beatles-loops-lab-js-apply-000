a41f140246643c6542e5e84ce186016acca0f4bf
Steven Chu <work@stevenchustudio.com>
2019-03-26 06:52:29 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -8,13 +8,13 @@ describe('Beatles Loops', function() {
 
   describe('johnLennonFacts', function(){
     it("returns an array of strings with exclamation points", function() {
-      /*expect(johnLennonFacts([
+      expect(johnLennonFacts([
         "He was the last Beatle to learn to drive",
         "He was never a vegetarian",
         "He was a choir boy and boy scout",
         "He hated the sound of his own voice"
       ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
-*/
+
       expect(johnLennonFacts([
         "foo",
         "bar",
