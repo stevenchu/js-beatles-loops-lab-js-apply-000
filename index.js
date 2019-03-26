0658c3dde036a44043dc08ccc0e43283086978bd
Steven Chu <work@stevenchustudio.com>
2019-03-26 06:53:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function theBeatlesPlay(musicians,instruments) {
   return output
 }
 
-const sayings = [
+/*const sayings = [
 "He was the last Beatle to learn to drive",
 "He was never a vegetarian",
 "He was a choir boy and boy scout",
 "He hated the sound of his own voice"
-]
+]*/
 
 function johnLennonFacts(facts) {
   var output = []
